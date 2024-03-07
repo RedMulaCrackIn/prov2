0846da68be092fbd30b9c4c88811d994a760c8d6
RedMulaCrackIn <giony2003@gmail.com>
2024-03-07 15:30:08 +0100

    Make a message

--- prov2/src/main/java/prov2/MainClass.java
+++ prov2/src/main/java/prov2/MainClass.java
@@ -6,7 +6,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("A message");
+		System.out.println("message");
 	}
 
 }
