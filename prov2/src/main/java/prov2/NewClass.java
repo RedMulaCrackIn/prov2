8cc8080e649df4ad40c05c55e119bed43d993a16
RedMulaCrackIn <giony2003@gmail.com>
2024-03-07 14:07:56 +0100

    Added a new class

--- prov2/src/main/java/prov2/NewClass.java
+++ prov2/src/main/java/prov2/NewClass.java
@@ -1,10 +1,10 @@
 package prov2;
 
-public class MainClass {
+public class NewClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("A message");
+
 	}
 
 }
